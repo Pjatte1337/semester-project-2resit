9e65a578f44b05c0cda00ffe938dfbc0a52f48b3
Joakim Vanebo <Joakimvanebo1@gmail.com>
2023-06-21 00:23:35 +0200

    added content for user

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React from "react";
 import DashboardLayout from "./../layouts/DashboardLayout";
 import { getUserName } from "../functions";
 
+const Dashboard = (props) => {
+  const userName = getUserName() ? getUserName() : "";
 
-const Dashboard = ( props ) => {
-
-	const userName = ( getUserName() ) ? getUserName() : '';
-
-	return(
-		<DashboardLayout>
-			{ userName ? <h2>Welcome { userName }!!</h2>: '' }
-		</DashboardLayout>
-	)
+  return (
+    <DashboardLayout>
+      <div className="center-content">
+        {userName ? (
+          <h2 className="welcome-heading">Welcome {userName}!!</h2>
+        ) : (
+          ""
+        )}
+        <p>
+          Hi, I'm Pjatte and I'm a software developer. I specialize in both
+          frontend and backend development, with a particular focus on web apps.
+          My experience includes developing blogs, wikis, and other web
+          applications. I love the challenge of creating something new from
+          scratch and am always looking for ways to improve my skillset. When I'm
+          not coding, you'll find me exploring the world of technology, reading
+          books and articles about the latest trends in software development.
+        </p>
+		<p>
+          Hi, I'm Pjatte and I'm a software developer. I specialize in both
+          frontend and backend development, with a particular focus on web apps.
+          My experience includes developing blogs, wikis, and other web
+          applications. I love the challenge of creating something new from
+          scratch and am always looking for ways to improve my skillset. When I'm
+          not coding, you'll find me exploring the world of technology, reading
+          books and articles about the latest trends in software development.
+        </p>
+      </div>
+    </DashboardLayout>
+  );
 };
 
 export default Dashboard;
