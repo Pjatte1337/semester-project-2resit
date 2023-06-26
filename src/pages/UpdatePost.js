b4b9eb665e7ea0548d50144eeae4c3413bdc4101
Joakim Vanebo <Joakimvanebo1@gmail.com>
2023-06-26 20:59:22 +0200

    removed one close button

--- src/pages/UpdatePost.js
+++ src/pages/UpdatePost.js
@@ -121,7 +121,7 @@ const UpdatePost = ({ postId }) => {
             contentClassName="modal-content"
             ref={modalRef}
           >
-            <Modal.Header closeButton>
+            <Modal.Header>
               <Modal.Title>Edit Post</Modal.Title>
             </Modal.Header>
             <Modal.Body>
